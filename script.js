014afa4857b56140e4642263d9558077be1ceedf
hsgwKei <63716217+hsgwKei@users.noreply.github.com>
2021-06-11 13:27:06 +0900

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-//const opencv = require('@mjyc/opencv.js/opencv.js')
-import cv from "@mjyc/opencv.js";
-var txt = document.getElementById('text1')
-
-window.onload = function() {
-    txt.textContent = "1"
-    cv.solvePnP();
-    txt.textContent = "2"
-}
