9a4ec039b35d7799dec31f0f84c304ee99bed3a4
hsgwKei <63716217+hsgwKei@users.noreply.github.com>
2021-06-14 13:46:09 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+//const opencv = require('@mjyc/opencv.js/opencv.js')
+import cv from "opencv.js";
+var txt = document.getElementById('text')
+
+window.onload = function() {
+    txt.textContent = "1"
+    cv.solvePnP();
+    txt.textContent = "2"
+}
